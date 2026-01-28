2b519da9179a91c3fba4de7c6e8155c45b4941b6
SongJongbeen <1041489@gmail.com>
2026-01-28 10:24:19 +0900

    new build with style

--- build_exe.bat
+++ build_exe.bat
@@ -3,8 +3,15 @@ echo PyInstaller로 exe 파일 빌드 중...
 pyinstaller --onedir ^
     --windowed ^
     --name CareNote ^
-    --hidden-import PyQt6 ^
-    --hidden-import qt_material ^
+    --hidden-import PyQt6.QtCore ^
+    --hidden-import PyQt6.QtWidgets ^
+    --hidden-import PyQt6.QtGui ^
+    --hidden-import PyQt6.QtPrintSupport ^
+    --hidden-import carenote.database ^
+    --hidden-import carenote.models ^
+    --hidden-import carenote.crud ^
+    --hidden-import carenote.gui ^
+    --hidden-import carenote.config ^
     --collect-all qt_material ^
     --collect-all PyQt6 ^
     --clean ^
